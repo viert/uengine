951394f5da8ba68b35e294509af3d0ca3b09c9dc
Pavel Vorobyov <aquavitale@yandex.ru>
2019-12-10 11:32:11 +0300

    exclude pyc files from package data

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="uengine",
-    version="3.1.14",
+    version="3.1.15",
     description="a micro webframework based on flask and pymongo",
     url="https://github.com/viert/uengine",
     author="Pavel Vorobyov",
@@ -11,6 +11,9 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        "uengine": ["*.txt", "*,py"]
+    },
     install_requires=[
         "jinja2",
         "flask",
