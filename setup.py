a3c401471f5f03446cc10b4f829f52b891469dd2
Pavel Vorobyov <aquavitale@yandex.ru>
2020-06-05 13:24:27 +0300

    Bump version: 3.6.1 → 3.6.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="uengine",
-    version="3.6.1",
+    version="3.6.2",
     description="a micro webframework based on flask and pymongo",
     url="https://github.com/viert/uengine",
     author="Pavel Vorobyov",
